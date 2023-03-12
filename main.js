d11f32e3caea0e3bd0c0309f9d9fe452b8560b05
Joshua Varghese <joshvarg.sec@gmail.com>
2023-03-12 15:15:46 -0400

    Fix to display game names

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-let url = "site.api.espn.com/apis/site/v2/sports/basketball/nba/scoreboard";
+let url = "https://site.api.espn.com/apis/site/v2/sports/basketball/nba/scoreboard";
 var req = new Request(url);
 req.headers = {"Content-type": "application/json;charset=UTF-8"};
 var res = await req.loadJSON();
 
 let games = res.events;
 let text = "";
-games.forEach(game => text.concat(game.name, " "));
+games.forEach(game => text = text.concat(game.name, " "));
 let w = new ListWidget();
 let t = w.addText(text);
 

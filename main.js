e8abcde8bb339d5d1f04c616e2900af77c6847d3
Joshua Varghese <joshvarg.sec@gmail.com>
2023-04-29 13:54:52 -0400

    feat: add status description and detail

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
-let url = "https://site.api.espn.com/apis/site/v2/sports/basketball/nba/scoreboard";
+let url = `https://site.api.espn.com/`+
+`apis/site/v2/sports/basketball/nba/scoreboard`;
+
 var req = new Request(url);
 req.headers = {"Content-type": "application/json;charset=UTF-8"};
 var res = await req.loadJSON();
 
 const lakers = new RegExp("[Ll]akers");
 const pistons = new RegExp("[Pp]istons");
+const suns = new RegExp("[Ss]uns");
+
 function checkGames(matchup, teamName){
   var bool = false;
   let found = matchup.match(teamName);
@@ -27,11 +31,14 @@ function teamPlaying(todayGames, teamName){
 
 let w = new ListWidget();
 
-let eventsidx = teamPlaying(res.events, pistons);
+let eventsidx = teamPlaying(res.events, suns);
 if (eventsidx > -1) {
   var game = res.events[eventsidx];
-  var name = game.name
-  let t = w.addText(name);
+  var sname = game.shortName;
+  var gstatus = game.status.type.description;
+  var time = game.status.type.shortDetail;
+  let t = w.addText(sname);
+  let y = w.addText(gstatus + ": " + time);
 }
 
 Script.setWidget(w);
